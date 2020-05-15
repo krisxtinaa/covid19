157b565ed7c7c80b25aedb295a6938c5d7c0f46c
Kristina Krasteva <i400369@fhict.nl>
2020-05-15 23:38:03 +0200

    Updates

---
+++ precache-manifest.47b8bd596cfed4b5254228db0ab3d4ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab51fec1c84966d1e3949f1776d262f",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "bd61c8df9accc98d5441",
+    "url": "/covid19/static/css/main.974d153a.chunk.css"
+  },
+  {
+    "revision": "ce43a6e4894ad3dfc51b",
+    "url": "/covid19/static/js/2.38b49f16.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19/static/js/2.38b49f16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd61c8df9accc98d5441",
+    "url": "/covid19/static/js/main.46485227.chunk.js"
+  },
+  {
+    "revision": "080958ad2f3d2ab682fd",
+    "url": "/covid19/static/js/runtime-main.6dbe199c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19/static/media/image.d7265326.png"
+  }
+]);
